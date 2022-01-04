ede3951b6fea227276f255b8f9bb6f96feb9aa04
Gabriel Souza <95205382+GSnery@users.noreply.github.com>
2022-01-04 12:41:55 -0300

    Add files via upload

---
+++ meu primeiro trabalho/js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
